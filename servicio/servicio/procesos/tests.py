f8bed4eff920ee0b59da91e04fe2fd4c07a8f8de
Richard Carrion <rcarrion3406@gmail.com>
2021-07-23 11:59:29 -0500

    Test unitarios endpoints

--- servicio/servicio/procesos/tests.py
+++ servicio/servicio/procesos/tests.py
@@ -4,20 +4,81 @@ from servicio.app import create_app
 import unittest
 
 
-class TestUsuariosActivos(unittest.TestCase):
+class TestProcesos(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.app = create_app()
+        self.tester = self.app.test_client()
+        self.usuario = "123"
+        self.proceso = "1"
+        self.usuario_data = [b"cedula_usuario", b"nombre_usuario", b"apellido_usuario"]
+        self.proceso_data = [b"id_proceso", b"nombre_proceso", b"fecha_creacion_proceso", b"estado_proceso"]
+        self.activo_data = [b"id_activo"]
+        self.item_data = [b"id_item", b"nombre_item", b"descripcion_item"]
 
-    # Prueba que el metodo devuelva una instancia de Usuario junto con un numero entero
-    def test_get_usuarios_cant_activos(self):
-        app = create_app()
-        with app.app_context():
-            resultado = get_usuarios_cant_activos()
-            for usuario, cant_activos in resultado:
-                self.assertIsInstance(usuario,Usuario) and self.assertIsInstance(cant_activos,int)
+    def __endpoint_test(self, ruta):
+        status_code = self.tester.get(ruta).status_code
+        self.assertEqual(status_code, 200)
 
-    def test_get_activos_por_usuario(self):
-        app = create_app()
-        with app.app_context():
-            usuario = get_usuario_por_cedula("123")
-            resultado = get_activos_por_usuario(usuario)
-            self.assertTrue(True)
+    def __content_type_test(self, ruta):
+        content_type = self.tester.get(ruta).content_type
+        self.assertEqual(content_type, "application/json")
+
+    def __data_test(self, ruta, data):
+        data_json = self.tester.get(ruta).data
+        for key in data:
+            print(key)
+            self.assertTrue(key in data_json)
+
+    def test_usuarios_cantidad_activos(self):
+        ruta = "/usuarios/cantidad-activos"
+        print("\n\n" + ruta)
+        self.__endpoint_test(ruta)
+        self.__content_type_test(ruta)
+        self.__data_test(ruta, self.usuario_data)
+
+    def test_activos_por_usuario(self):
+        ruta = f"/usuarios/{self.usuario}/activos"
+        print("\n\n" + ruta)
+        self.__endpoint_test(ruta)
+        self.__content_type_test(ruta)
+        self.__data_test(ruta, self.activo_data + self.item_data)
+
+    def test_procesos_por_usuario(self):
+        ruta = f"/usuarios/{self.usuario}/procesos"
+        print("\n\n" + ruta)
+        self.__endpoint_test(ruta)
+        self.__content_type_test(ruta)
+        self.__data_test(ruta, self.proceso_data)
+
+    def test_procesos(self):
+        ruta = "/procesos"
+        print("\n\n" + ruta)
+        self.__endpoint_test(ruta)
+        self.__content_type_test(ruta)
+        self.__data_test(ruta, self.proceso_data)
+
+    def test_usuarios_por_procesos(self):
+        ruta = f"/procesos/{self.proceso}/usuarios"
+        print("\n\n" + ruta)
+        self.__endpoint_test(ruta)
+        self.__content_type_test(ruta)
+        self.__data_test(ruta, self.usuario_data)
+
+    def test_activos_por_procesos(self):
+        ruta = f"/procesos/{self.proceso}/activos"
+        print("\n\n" + ruta)
+        self.__endpoint_test(ruta)
+        self.__content_type_test(ruta)
+        self.__data_test(ruta, self.activo_data + self.item_data)
+
+    def test_detalle_proceso(self):
+        ruta = f"/procesos/{self.proceso}"
+        print("\n\n" + ruta)
+        self.__endpoint_test(ruta)
+        self.__content_type_test(ruta)
+        self.__data_test(ruta, self.activo_data + self.item_data + self.proceso_data + self.usuario_data)
+
+    def test_index(self):
+        self.__endpoint_test("/")
