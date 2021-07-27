65214e2653aff9a70d5dcb9f1cd3c2d08b78b775
Richard Carrion <rcarrion3406@gmail.com>
2021-07-27 14:17:46 -0500

    Declaracion de views

--- clienteandroid/app/src/main/java/com/example/cliente_android/DetalleProcesoActivity.java
+++ clienteandroid/app/src/main/java/com/example/cliente_android/DetalleProcesoActivity.java
@@ -2,9 +2,11 @@ package com.example.cliente_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class DetalleProcesoActivity extends Activity {
+    TextView tvNombre, tvFecha, tvId, tvEstado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
